17bdbc407d1f8e007130c3a4c4a33ee9659129dc
Venuchander <venuchander1@gmail.com>
2025-01-28 20:37:34 +0530

    Fix Tailwind

--- client/src/adminEntry.jsx
+++ client/src/adminEntry.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Admin from './pages/Admin' // Adjust path as needed
+import Admin from './pages/Admin'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
